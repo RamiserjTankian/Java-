5897190b441fd023e65b0a69748a9fa5d5da88e0
RamiserjTankian <milinchi_@hotmail.com>
2016-08-25 11:29:22 -0500

    Update Practica1
    Agregados matrices

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -19,7 +19,13 @@ public class Practica1 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
+        
         float []estudiantes = new float [50];
+        int []cedula = new int [50];
+        float []genero = new float [50];
+        float []trabajo = new float [50];
+        float []sueldo = new float [50];
+        
         int estu= 0;
         System.out.print("Ingresa el numero de estudiantes a registrar: ");
         estu = scanner.nextInt();
@@ -28,8 +34,13 @@ public class Practica1 {
         }else
              System.out.print("Imposible registrar mas de 50 estudiantes");
                              
-         
-        
+        System.out.print("Ingresa los datos por orden (ENTER) para continuar:");
+           for (int c=0;c<estu;c++);
+           System.out.print("Ingresa la CEDULA");
+           cedula [c] = scanner.nextInt();
+           
+           
+                
 
 
 
